4cf2550af97c576e283cb1d2f3b10494d2d96d8a
Hughgo <go8105@gmail.com>
2024-10-25 09:54:20 +0900

    Add files via upload

---
+++ Prinics/src/main/java/com/example/Prinics/PrinicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Prinics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrinicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrinicsApplication.class, args);
+	}
+
+}
